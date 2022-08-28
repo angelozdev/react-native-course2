ca92277d80bdb670c1cf202e2897d152589c1c9a
Angelo Zambrano <angelozam17@gmail.com>
2022-08-28 10:17:09 -0500

    :tada: calculator: initial commit

--- 1-first-app/src/screens/homescreen.tsx
+++ 1-first-app/src/screens/homescreen.tsx
@@ -4,62 +4,78 @@ import {
   Text,
   View,
   useWindowDimensions,
-  Platform
+  Platform,
+  TextInput
 } from 'react-native'
 import React from 'react'
 import { Button } from '../features/ui'
 
-const STEP = 10
 export default function HomeScreen() {
   const [count, setCount] = React.useState(0)
+  const [step, setStep] = React.useState(10)
   const { width } = useWindowDimensions()
 
   const increment = () => {
     setCount((prevCount) => {
-      if (prevCount >= STEP * 5) return prevCount
-      return prevCount + STEP
+      if (prevCount >= step * 5) return prevCount
+      return prevCount + step
     })
   }
   const decrement = () => {
     if (count <= 0) return
     setCount((prevCount) => {
-      const newCount = prevCount - STEP
+      const newCount = prevCount - step
       if (newCount < 0) return 0
       return newCount
     })
   }
   const reset = () => setCount(0)
 
+  const handleStepChange = (text: string) => {
+    const newStep = parseInt(text, 10)
+    if (isNaN(newStep)) return setStep(10)
+    setStep(newStep)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Counter: {count}</Text>
-      <View style={styles.buttonsContainer}>
-        <View style={styles.inlineButtons}>
+      <View style={styles.content}>
+        <TextInput
+          keyboardType="number-pad"
+          style={{ width: '100%' }}
+          placeholder="Steps"
+          accessibilityRole="search"
+          onChangeText={handleStepChange}
+        />
+        <Text style={styles.title}>Counter: {count}</Text>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.inlineButtons}>
+            <Button
+              style={{ marginRight: width * 0.01, width: '49%' }}
+              disabled={count === step * 5}
+              onPress={increment}
+            >
+              Increment
+            </Button>
+            <Button
+              style={{ marginLeft: width * 0.01, width: '49%' }}
+              disabled={count === 0}
+              onPress={decrement}
+            >
+              Decrement
+            </Button>
+          </View>
           <Button
-            style={{ marginRight: width * 0.01 }}
-            disabled={count === STEP * 5}
-            onPress={increment}
-          >
-            Increment
-          </Button>
-          <Button
-            style={{ marginLeft: width * 0.01 }}
+            style={{ marginTop: width * 0.02 }}
             disabled={count === 0}
-            onPress={decrement}
+            onPress={reset}
           >
-            Decrement
+            Reset
           </Button>
         </View>
-        <Button
-          style={{ marginTop: width * 0.02 }}
-          disabled={count === 0}
-          onPress={reset}
-        >
-          Reset
-        </Button>
-      </View>
 
-      <Text>Platform: {Platform.OS}</Text>
+        <Text>Platform: {Platform.OS}</Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -78,6 +94,11 @@ const styles = StyleSheet.create({
     marginVertical: 20
   },
   inlineButtons: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  content: {
+    width: '100%',
+    padding: 20
   }
 })
---
+++ 2-calculator/App.tsx
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
